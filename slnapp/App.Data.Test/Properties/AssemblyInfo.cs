55412e7fa56a1887a1712d3fd1208f916cde3491
EXTENSION <eddygeronimo2016@gmail.com>
2019-04-14 12:06:35 -0500

    agregar proyecto 1

---
+++ slnapp/App.Data.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.Data.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.Data.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("369fa2c5-2e4b-410b-9df1-69ca0a6855f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ slnapp/App.Data/BaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data
+{
+    public class BaseConnection
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                string cnx = @"Data Source=MI607-ST\SQL2016PIVOT;Initial Catalog=dbchinok;user Id=chinok;
+                               Password=P@@w0rd";
+
+                return cnx;
+            }
+        }
+
+       
+    }
+}
